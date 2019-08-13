a4531e01c034cb5e2dd295339a1fa58f0aa10f6c
Wouter Milants <woutmilants@gmail.com>
2019-08-13 22:31:07 +0200

    added @EnableAutoConfiguration to generate database tables automatically

--- src/main/java/be/milants/expenseanalyzer/ExpenseanalyzerApplication.java
+++ src/main/java/be/milants/expenseanalyzer/ExpenseanalyzerApplication.java
@@ -1,9 +1,11 @@
 package be.milants.expenseanalyzer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ExpenseanalyzerApplication {
 
 	public static void main(String[] args) {
