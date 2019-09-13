c02b0a2f0387c6e4fa470698e2a09fd62c4ca178
wmilants <wouter.milants@esaturnus.com>
2019-09-13 15:03:07 +0200

    added fields to dto

--- src/main/java/be/milants/expenseanalyzer/expense/rest/model/ExpenseDto.java
+++ src/main/java/be/milants/expenseanalyzer/expense/rest/model/ExpenseDto.java
@@ -1,4 +1,23 @@
 package be.milants.expenseanalyzer.expense.rest.model;
 
+import be.milants.expenseanalyzer.data.Direction;
+
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
 public class ExpenseDto {
+    private Long id;
+    private String accountNumber;
+    private String accountName;
+    private String currency;
+    private Date date;
+    private String description;
+    private String currentBalance;
+    private Integer amountInCents;
+    private Direction direction;
+    private String counterPartAccount;
+    private String counterPartName;
+    private String statement;
 }
