4f25d32af08cb393ae74c599595499a456fb8bb6
wmilants <wouter.milants@esaturnus.com>
2019-09-13 15:06:10 +0200

    added annotations for serialization

--- src/main/java/be/milants/expenseanalyzer/expense/rest/model/ExpenseDto.java
+++ src/main/java/be/milants/expenseanalyzer/expense/rest/model/ExpenseDto.java
@@ -1,12 +1,20 @@
 package be.milants.expenseanalyzer.expense.rest.model;
 
 import be.milants.expenseanalyzer.data.Direction;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ExpenseDto {
     private Long id;
     private String accountNumber;
